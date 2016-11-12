6707cd16e1c92f04fc9ddc8fc59696d9b4f1e4be
gergalyb <gergalyb@gmail.com>
2016-11-12 13:08:35 +0100

    tooltip fix, updates
    tooltip fix, updated bootstrap and metarial design

--- js/init.js
+++ js/init.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     $.material.init();
   }, false)
+
+  $(document).ready(function() {
+      $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+  });
